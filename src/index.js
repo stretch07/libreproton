7baa199dc882553dd29098870945f337962c9216
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-04-28 22:22:55 +0000

    u

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import aes from "crypto-js/aes.js"
+import CryptoJS from "crypto-js"
+const aes = CryptoJS.AES
 const encrypt = aes.encrypt
 const decrypt = aes.decrypt
 
-console.log(encrypt("balls", "wow"))
+console.log(decrypt(encrypt("SECRET", "wow"), "wow").toString(CryptoJS.enc.Utf8))
