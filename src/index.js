b7246429a016aa012c3876cb1120be67edd42c84
its-pablo <78528552+its-pablo@users.noreply.github.com>
2023-05-20 00:34:47 +0000

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import CryptoJS from "crypto-js"
-const aes = CryptoJS.AES
-const encrypt = aes.encrypt
-const decrypt = aes.decrypt
-
-console.log(decrypt(encrypt("SECRET", "wow"), "wow").toString(CryptoJS.enc.Utf8))
+import * as openpgp from "openpgp/lightweight"
